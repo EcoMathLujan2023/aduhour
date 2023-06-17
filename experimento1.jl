db2b1ee809eb3b54cf09e3b60ac69e23f442b4e2
Andrés Duhour <aeduhour@yahoo.com.ar>
2023-06-17 18:32:00 -0300

    agrego ejercicios día 1

--- experimento1.jl
+++ experimento1.jl
@@ -1,173 +1,8 @@
-# 1) Activar el proyecto de la carpeta actual: `activate .`
-# 2) Qué paquetes vamos a usar.
-# 2.1) `add Plots`
+## Crecimiento exponencial deterministico discreto 
 
+# asume intervalo de tiempo 1 (discreto)
 
-# Variables.
-
-x = 10
-
-typeof(x) # qué tipo de variable es x
-
-x=10.0
-
-typeof(x)
-
-y = 10
-
-x + y
-
-# Vectores y matrices
-
-# Inicializar un vector o una matriz
-
-x = [10, 10, 10, 10, 11]
-
-x = zeros(10)
-
-x = [Float64]
-
-x = zeros(3,4)
-
-mat = [10 11; 12 13]
-
-X = [1:4]
-
-# Julia toma la notación matemática
-
-# Multiplicar escalares por Vectores
-
-5 * x 
-
-# Las operaciones no definidas para un vector 
-# se realizan agregando un punto.
-
-log(x) # Error
-
-log.(x)
-
-# Condicionales
-
-if length(x) == 1
-    @info "El vector es de largo 1"
-else 
-    @info "El vector es mayor que 1"
-end
-
-if length(x) == 1
-    @info "El vector es de largo 1"
-elseif length(x) == 2
-    @info "El vector es de largo 2"
-else @info "EL vector es mayor que 2"
-end
-
-if typeof(x) == "Vector"
-    @info "El tipo es vector"
-else
-    @info "El tipo de x es $(typeof(x))"
-end
-
-# Bucle
-
-for i in 1: 10
-    @info "i = $(i)"
-end
-
-# Combinar bucles y Condicionales
-
-for i in 1:10
-    if i == 5
-        @info "Ohhhhh i es igual a 5"
-    else
-        @info "i es igual a $(i)"
-    end   
-end
-
-x = rand(10)
-
-for i in 1:10
-    if x[i]<0.2
-        @info "Evaluamos la probabilidad de 0.2"
-    end
-end
-
-p = 0.2 
-
-for i in 1:10
-    if rand() < p 
-        @info "Verdadero"
-    else
-        @info "Falso"
-    end
-end
-
-# Funciones
-
-function evento_aleatorio(p)
-    if p > 1
-        exit
-    end
-    if rand() > p
-        return true
-    else
-        return false
-    end
-end
-eventos = Bool[]
-
-typeof(eventos)
-
-
-push!(eventos,false)
-
-for i in 1:100
-    push!(eventos, evento_aleatorio(0.1))
-end
-
-eventos
-
-count(eventos)
-
-## Hacer una función caminante aleatorio.
-## que nos diga si el caminante dio un paso 
-## hacia adelante o hacia atrás.
-
-function pos_caminante(p)
-    if evento_aleatorio(p) == true
-        @info "Adelante"
-    else
-        @info "Atrás"
-    end
-end
-
-pos_caminante(0.98)
-
-
-pasos = 100
-function pos_caminante2(p,pasos)
-    caminata_aleatoria = Int16[]
-    for i in 1:pasos
-        if evento_aleatorio(p) == true
-            push!(caminata_aleatoria,1)
-        else
-            push!(caminata_aleatoria,-1)
-        end
-    end
-    return caminata_aleatoria
-end 
-
-eventos = pos_caminante2(0.5,pasos)
-sum(eventos)
-
-## Gráficos
-#
-
-using Plots
-
-plot(eventos)
-scatter!(eventos,legend=false)
-
-function crecimiento_exponencial(lambda, N0,tfinal) # asume intervalo de tiempo 1
+function crecimiento_exponencial(lambda, N0,tfinal) 
     pop = [N0]
 
     for t in 1:tfinal-1
@@ -192,3 +27,52 @@ plot!(p3)
 ## Simulación exponencial 
 
 
+## Ejercicios. 
+
+# Dos numerales define un bloque de código 
+#  que se puede ejecutar con una Combinacíon de teclas
+
+# Crecimiento exponencial determinístico
+# discreto con intervalo de tiempo h variable.
+# Modificando la función anterior. 
+# Subirlo al repositorio de Github 
+
+function crec_exp(λ,N0,tfinal,h=1)
+    pop = [N0]
+    ts = [0.0]
+    i = 1
+    t = 0.0
+    while t <= tfinal
+        pop1 = pop[i] + h * λ * pop[i]
+        t = ts[i] + h
+        push!(pop,pop1)
+        push(ts,t)
+    end
+    return pop, ts
+end
+
+c1 = crec_exp(0.1,1-0,100,1)
+
+plot(c1)
+function distr_exp(λ)
+    n = 10000
+    de = zeros(n)
+    for i in 1:n
+    de[i] = -log(rand())/λ
+    end
+    return de
+end
+
+de = distr_exp(10*2/100)
+plot(de)
+
+# Para graficar la densidad de la distribución
+using StatsPlots
+density(de)
+
+function promedio (x)
+    p = sum(x)/length(x)
+    return p
+end
+
+sum(de)/length(de)
